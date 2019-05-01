5dc170766c6a3eff0bca99d3da6bcdb07ce2ea7d
clemens_latop <clemens.sonnleitner@nav.uni-stuttgart.de>
2019-05-01 18:38:23 +0200

    bugfix

--- Python/clemens_test.py
+++ Python/clemens_test.py
@@ -75,11 +75,12 @@ fileending=1
 while os.path.isfile('/home/pi/Navio2/Python/testrun_{}_IMU.txt'.format(fileending)) is True:
     fileending += 1
 
-# write headers to file
-with open('/home/pi/Navio2/Python/testrun_{}_IMU.txt'.format(fileending), 'w') as dat_imu, \
-        open('/home/pi/Navio2/Python/testrun_{}_GNSS.txt'.format(fileending), 'w') as dat_gnss, \
-        open('/home/pi/Navio2/Python/testrun_{}_baro.txt'.format(fileending), 'w') as dat_baro:
+# open(-, -, 1) enables line buffering
+with open('/home/pi/Navio2/Python/testrun_{}_IMU.txt'.format(fileending), 'w', 1) as dat_imu, \
+        open('/home/pi/Navio2/Python/testrun_{}_GNSS.txt'.format(fileending), 'w', 1) as dat_gnss, \
+        open('/home/pi/Navio2/Python/testrun_{}_baro.txt'.format(fileending), 'w', 1) as dat_baro:
 
+    # write headers to file
     dat_imu.write('t[s], mpu_accel_1, mpu_accel_2, mpu_accel_3, mpu_gyro_1, mpu_gyro_2, mpu_gyro_3, '
                   'mpu_magn_1, mpu_magn_2, mpu_magn_3, '
                   'lsm_accel_1, lsm_accel_2, lsm_accel_3, lsm_gyro_1, lsm_gyro_2, lsm_gyro_3, '
@@ -87,58 +88,38 @@ with open('/home/pi/Navio2/Python/testrun_{}_IMU.txt'.format(fileending), 'w') a
     dat_gnss.write('t[s], iTOW, ecefX [cm], ecefY [cm], ecefZ [cm], pAcc\n')
     dat_baro.write('t[s], pressure [mbar], temperature [°C]\n')
 
+    # Main loop
 
-# Main loop
-# As Power loss seems to corrupt the files the files are appended in batches
-t_l1 = 0.0  # time of last gnss & baro measurement
-t_l2 = 0.0 # time of last write to file
-while True:
-    t_a = time.time() - t_s
-    imudata = []
-    gnssdata = []
-    barodata = []
+    t_l = 0.0  # time of last gnss & baro measurement
+    while True:
+        t_a = time.time() - t_s
 
-    mpudata_a, mpudata_g, mpudata_m = mpu.getMotion9()
-    lsmdata_a, lsmdata_g, lsmdata_m = lsm.getMotion9()
+        mpudata_a, mpudata_g, mpudata_m = mpu.getMotion9()
+        lsmdata_a, lsmdata_g, lsmdata_m = lsm.getMotion9()
 
-    # GNSS & barometer
-    if t_a - t_l1 > 1.0:
-        t_l1 = t_a
-        baro.update()
-        msg = ubl.receive_message()
-        if msg is None:
-            if opts.reopen:
-                ubl.close()
-                ubl = ublox.UBlox("spi:0.0", baudrate=5000000, timeout=2)
-                continue
-            print(empty)
-            break
-        if msg.name() == "NAV_POSECEF":
-            gnssdata.append("{}, {}".format(t_a, str(struct.unpack('<IiiiI', msg._buf[6:26])).replace("(", "").replace(")", "")))
+        # GNSS & barometer
+        if t_a - t_l > 1.0:
+            t_l = t_a
+            baro.update()
+            msg = ubl.receive_message()
+            if msg is None:
+                if opts.reopen:
+                    ubl.close()
+                    ubl = ublox.UBlox("spi:0.0", baudrate=5000000, timeout=2)
+                    continue
+                print(empty)
+                break
+            if msg.name() == "NAV_POSECEF":
+                dat_gnss.append("{}, {}".format(t_a, str(struct.unpack('<IiiiI', msg._buf[6:26])).replace("(", "").replace(")", "")))
+                # dat_gnss.flush()
 
-        barodata.append("{}, {}, {}".format(t_a, baro.returnPressure(), baro.returnTemperature()))
+            dat_baro.append("{}, {}, {}".format(t_a, baro.returnPressure(), baro.returnTemperature()))
+            # dat_baro.flush()
 
+        data = [t_a] + mpudata_a + mpudata_g + mpudata_m + lsmdata_a + lsmdata_g + lsmdata_m
 
-    data = [t_a] + mpudata_a + mpudata_g + mpudata_m + lsmdata_a + lsmdata_g + lsmdata_m
+        # print(data)
+        dat_imu.write(str(data).replace("[", "").replace("]", ""))
+        # dat_imu.flush()
 
-    # print(data)
-    imudata.append(str(data).replace("[", "").replace("]", ""))
-
-    # Write data to file every 10 seconds
-    if t_a - t_l2 > 10.0:
-        t_l2 = t_a
-        with open('/home/pi/Navio2/Python/testrun_{}_IMU.txt'.format(fileending), 'a') as dat_imu, \
-                open('/home/pi/Navio2/Python/testrun_{}_GNSS.txt'.format(fileending), 'a') as dat_gnss, \
-                open('/home/pi/Navio2/Python/testrun_{}_baro.txt'.format(fileending), 'a') as dat_baro:
-            for l in imudata:
-                dat_imu.write(l + "\n")
-            for l in gnssdata:
-                dat_gnss.write(l + "\n")
-            for l in barodata:
-                dat_baro.write(l + "\n")
-        imudata = []
-        gnssdata = []
-        barodata = []
-
-    else:
         time.sleep(0.005) # sleep time to restrict update rate
