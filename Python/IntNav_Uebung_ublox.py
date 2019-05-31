8d5cd18eb491a9fe1a7574eddb9093e657eb0647
clemens_latop <clemens.sonnleitner@nav.uni-stuttgart.de>
2019-05-31 13:45:51 +0200

    bugfix

--- Python/IntNav_Uebung_ublox.py
+++ Python/IntNav_Uebung_ublox.py
@@ -32,7 +32,7 @@ def main():
 
         while True:
             msg = ubl.receive_message()
-            if type(msg) is None:
+            if msg is None:
                 continue
             elif msg.name() == "ESF-RAW":
                 dat_ESF.write(msg)
